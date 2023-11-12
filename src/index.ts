b912d918e441bcd3c7efbd72b102af529eabbf6e
longdv ph29867 <emchuaroinhe@gmail.com>
2023-11-12 21:30:40 +0700

    update

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import express, { Request, Response } from 'express'
-const app = express()
-
-app.get('/', (req: Request, res: Response) => {
-  res.sendStatus(200)
-})
-
-app.listen(8000, () => console.log('123'))
