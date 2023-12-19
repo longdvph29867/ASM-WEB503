dcf9cad4c83f6db300d21f15304fc168c020b25d
longdv ph29867 <emchuaroinhe@gmail.com>
2023-12-20 05:31:03 +0700

    update

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,21 @@
 /**
  * @swagger
+ * /posts:
+ *   get:
+ *     summary: Returns all posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: the list of the posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
+/**
+ * @swagger
  * tags:
  *   name: Auth
  *   description: API operations related to auth
