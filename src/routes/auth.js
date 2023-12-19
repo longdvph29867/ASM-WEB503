603d576a1389ad5db589208b2a7ed794c83d90f2
longdv ph29867 <emchuaroinhe@gmail.com>
2023-12-20 06:33:49 +0700

    update

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,60 @@
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - userId
+ *         - title
+ *         - body
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The Auto-generated id of a post
+ *         userId:
+ *           type: integer
+ *           description: id of author
+ *         title:
+ *           type: string
+ *           description: title of post
+ *         body:
+ *           type: string
+ *           descripton: content of post
+ *       example:
+ *         id: 1
+ *         userId: 1
+ *         title: my title
+ *         body: my article
+ *
+ */
+
+/**
+ * @swagger
+ *  tags:
+ *    name: Posts
+ *    description: posts of users
+ */
+
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Returns all posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: the list of the posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
+
+/**
+ * @swagger
  * tags:
  *   name: Auth
  *   description: API operations related to auth
