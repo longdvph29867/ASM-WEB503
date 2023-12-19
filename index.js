e5ea1eefcbfdf10a111add9e0687e3d32bf8437a
longdv ph29867 <emchuaroinhe@gmail.com>
2023-12-20 06:21:15 +0700

    update

--- index.js
+++ index.js
@@ -16,36 +16,60 @@ app.use(express.json());
 app.use(cors());
 connectDB(URI_DB);
 
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Express API with Swagger",
+//       version: "0.1.0",
+//       description: "My API Documentation",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:8000/",
+//       },
+//       {
+//         url: "https://asm-web-503.vercel.app/",
+//       },
+//     ],
+//     components: {
+//       securitySchemes: {
+//         JWT: {
+//           type: "apiKey",
+//           in: "header",
+//           name: "Authorization",
+//         },
+//       },
+//     },
+//   },
+//   apis: ["./src/**/*.js"],
+//   cors: true,
+// };
+
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Express API with Swagger",
-      version: "0.1.0",
-      description: "My API Documentation",
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
     },
     servers: [
       {
-        url: "http://localhost:8000/",
-      },
-      {
-        url: "https://asm-web-503.vercel.app/",
+        url: "https://nodejs-swagger-api.vercel.app/",
+        description: "My API Documentation",
       },
     ],
-    components: {
-      securitySchemes: {
-        JWT: {
-          type: "apiKey",
-          in: "header",
-          name: "Authorization",
-        },
-      },
-    },
   },
-  apis: ["./src/**/*.js"],
-  cors: true,
+  // This is to call all the file
+  apis: ["src/**/*.js"],
 };
-
 const specs = swaggerJsdoc(options);
 app.use(
   "/api-docs",
