33ae176ff83a62b2378bf3a74617b494144fae7b
longdv ph29867 <emchuaroinhe@gmail.com>
2023-12-21 21:21:15 +0700

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ connectDB(URI_DB);
 
 const options = {
   definition: {
-    openapi: "3.0.0",
+    openapi: "3.1.0",
     info: {
       title: "Express API with Swagger",
       version: "0.1.0",
